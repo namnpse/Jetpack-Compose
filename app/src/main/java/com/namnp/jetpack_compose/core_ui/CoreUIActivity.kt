5bfe42859dcd55d04a39ecd6b2a6209ae0ad5232
Namnpse <namnpse@gmail.com>
2023-12-30 14:33:19 +0900

    row composable, horizontal arrangement, vertical alignment

--- app/src/main/java/com/namnp/jetpack_compose/core_ui/CoreUIActivity.kt
+++ app/src/main/java/com/namnp/jetpack_compose/core_ui/CoreUIActivity.kt
@@ -3,15 +3,22 @@ package com.namnp.jetpack_compose.core_ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class CoreUIActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,13 +38,28 @@ fun MainScreen() {
     ) {
         Surface(
             color = Color.Green,
-            modifier = Modifier.wrapContentSize(),
+            modifier = Modifier.wrapContentSize(
+                align = Alignment.BottomCenter
+            ),
         ){
-            Text(
-                text = "Wrapped content",
+            Row(
+                modifier = Modifier.size(300.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+//                    color = Color.Red,
+                    modifier = Modifier.background(Color.Red),
+                    text = "Wrapped content",
 //                modifier = Modifier.wrapContentSize(), default is wrap content -> can be removed
-                style = MaterialTheme.typography.headlineLarge,
-            )
+                    style = MaterialTheme.typography.headlineLarge,
+                )
+                Text(
+                    modifier = Modifier.background(Color.Yellow),
+                    text = "a",
+                    style = MaterialTheme.typography.headlineLarge,
+                )
+            }
         }
     }
 }
