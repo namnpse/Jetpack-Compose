15919c026ff440e0d26a5885e78e4fb52261f6a2
Namnpse <nam444479@gmail.com>
2024-01-02 23:52:48 +0900

    add Retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,12 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+
+    // Retrofit
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
