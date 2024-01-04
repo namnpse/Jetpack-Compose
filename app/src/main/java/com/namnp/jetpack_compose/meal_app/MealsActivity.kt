084eb92abceedd64ce8d630cf5ae425610873c90
Namnpse <nam444479@gmail.com>
2024-01-04 21:24:44 +0900

    add meal activity, handle navigate from list meals screen to meal details screen

---
+++ app/src/main/java/com/namnp/jetpack_compose/meal_app/MealsActivity.kt
@@ -0,0 +1,47 @@
+package com.namnp.jetpack_compose.meal_app
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.namnp.jetpack_compose.meal_app.meal_details.MealDetailsScreen
+import com.namnp.jetpack_compose.meal_app.meal_details.MealDetailsViewModel
+import com.namnp.jetpack_compose.ui.theme.JetpackComposeTheme
+
+class MealsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                MealsApp()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MealsApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "meals_list") {
+        composable(route = "meals_list") {
+            MealsCategoriesScreen { navigationMealId ->
+                navController.navigate("meal_details/$navigationMealId")
+            }
+        }
+        composable(
+            route = "meal_details/{meal_category_id}",
+            arguments = listOf(navArgument("meal_category_id") {
+                type = NavType.StringType
+            })
+        ) {
+            val viewModel: MealDetailsViewModel = viewModel()
+            MealDetailsScreen(viewModel.mealState.value)
+        }
+    }
+}
--- app/src/main/java/com/namnp/jetpack_compose/meal_app/MealsCategoriesScreen.kt
+++ app/src/main/java/com/namnp/jetpack_compose/meal_app/MealsCategoriesScreen.kt
@@ -1,6 +1,8 @@
 package com.namnp.jetpack_compose.meal_app
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,14 +12,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,26 +37,31 @@ import com.namnp.jetpack_compose.meal_app.model.response.MealResponse
 import com.namnp.jetpack_compose.ui.theme.JetpackComposeTheme
 
 @Composable
-fun MealsCategoriesScreen() {
+fun MealsCategoriesScreen(onMealItemClick: (String) -> Unit) {
     val viewModel: MealsCategoriesViewModel = viewModel()
     val meals = remember { viewModel.mealsState.value }
     LazyColumn(contentPadding = PaddingValues(16.dp)) {
         items(meals) { meal ->
-            MealCategory(meal)
+            MealCategory(meal, onMealItemClick)
         }
     }
 }
 
 @Composable
-fun MealCategory(meal: MealResponse) {
+fun MealCategory(meal: MealResponse, onMealItemClick: (String) -> Unit) {
+    var isExpanded by remember { mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 16.dp)
+            .clickable {
+                onMealItemClick(meal.id)
+            }
     ) {
-        Row {
+        // animate content size
+        Row(modifier = Modifier.animateContentSize()) {
             Image(
                 painter = rememberAsyncImagePainter(meal.imageUrl),
                 contentDescription = null,
@@ -62,7 +78,30 @@ fun MealCategory(meal: MealResponse) {
                     text = meal.name,
                     style = MaterialTheme.typography.titleMedium
                 )
+                Text(
+                    text = meal.description,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.titleMedium,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = if (isExpanded) 10 else 4
+                )
             }
+            Icon(
+                imageVector = if (isExpanded)
+                    Icons.Filled.KeyboardArrowUp
+                else
+                    Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(
+                        if (isExpanded)
+                            Alignment.Bottom
+                        else
+                            Alignment.CenterVertically
+                    )
+                    .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
@@ -71,6 +110,6 @@ fun MealCategory(meal: MealResponse) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        MealsCategoriesScreen()
+        MealsCategoriesScreen({})
     }
 }
