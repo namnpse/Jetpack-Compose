0a8aaf2d3e285ad38aefcb37193db6a5a9079ec4
Namnpse <nam444479@gmail.com>
2024-01-08 23:59:57 +0900

    build bottom navigation composable in meditation app

---
+++ app/src/main/java/com/namnp/jetpack_compose/meditation_app/components/BottomNavigationMenu.kt
@@ -0,0 +1,54 @@
+package com.namnp.jetpack_compose.meditation_app.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.namnp.jetpack_compose.meditation_app.model.BottomMenuModel
+import com.namnp.jetpack_compose.ui.theme.AquaBlue
+import com.namnp.jetpack_compose.ui.theme.ButtonBlue
+import com.namnp.jetpack_compose.ui.theme.DeepBlue
+
+@Composable
+fun BottomNavigationMenu(
+    items: List<BottomMenuModel>,
+    modifier: Modifier = Modifier,
+    activeHighlightColor: Color = ButtonBlue,
+    activeTextColor: Color = Color.White,
+    inactiveTextColor: Color = AquaBlue,
+    initialSelectedIndex: Int = 0
+) {
+    var selectedIndex by remember {
+        mutableIntStateOf(initialSelectedIndex)
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(DeepBlue)
+            .padding(15.dp)
+    ) {
+        items.forEachIndexed { index, item ->
+            BottomMenuItem(
+                item = item,
+                isSelected = index == selectedIndex,
+                activeHighlightColor = activeHighlightColor,
+                activeTextColor = activeTextColor,
+                inactiveTextColor = inactiveTextColor
+            ) {
+                selectedIndex = index
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/namnp/jetpack_compose/meditation_app/model/BottomMenuModel.kt
@@ -0,0 +1,8 @@
+package com.namnp.jetpack_compose.meditation_app.model
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuModel(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
