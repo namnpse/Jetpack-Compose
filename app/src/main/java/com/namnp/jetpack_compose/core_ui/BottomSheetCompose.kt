0f2894fa385937d2e63cab038cf609508ceda98e
Namnpse <nam444479@gmail.com>
2024-01-14 19:26:19 +0900

    show bottom sheet in Compose

---
+++ app/src/main/java/com/namnp/jetpack_compose/core_ui/BottomSheetCompose.kt
@@ -0,0 +1,69 @@
+package com.namnp.jetpack_compose.core_ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetCompose() {
+    val sheetState = rememberStandardBottomSheetState(
+        initialValue = SheetValue.Hidden
+    )
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+    val scope = rememberCoroutineScope()
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "BottomSheet",
+                    fontSize = 60.sp
+                )
+            }
+        },
+        sheetPeekHeight = 0.dp,
+        containerColor = Color.Green,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = {
+                scope.launch {
+                    if(sheetState.isVisible) {
+                        sheetState.hide()
+                    } else {
+                        sheetState.expand()
+                    }
+                }
+            }) {
+                Text(text = "Bottom sheet currentValue: ${sheetState.currentValue}")
+            }
+        }
+    }
+}
