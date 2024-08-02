e6c3aa491f0edd194d60aa87428125d7e63295cf
Namnpse <nam444479@gmail.com>
2024-08-02 20:50:58 +0900

    manage dependencies by libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,8 +83,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
+// Navigation
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+// Official Navigation Compose Lib
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
     // Retrofit
     implementation("com.google.code.gson:gson:2.10.1")
