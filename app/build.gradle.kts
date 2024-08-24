a6c1adbb9e95fbfea394e3e5e7f20206d3526fab
Namnpse <nam444479@gmail.com>
2024-08-24 18:07:55 +0900

    update minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.namnp.jetpack_compose"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
